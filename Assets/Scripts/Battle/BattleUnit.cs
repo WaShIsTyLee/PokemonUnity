eb1df836bfadb8370d4578c0a3fa5ca7dfb6caa6
Frenadol <fernandoramajimenez@gmail.com>
2024-10-18 19:18:21 +0200

    Encapsulamiento del combate

--- Assets/Scripts/Battle/BattleUnit.cs
+++ Assets/Scripts/Battle/BattleUnit.cs
@@ -7,10 +7,15 @@ using DG.Tweening;
 
 public class BattleUnit : MonoBehaviour
 {
-    
     [SerializeField] bool isPlayerUnit;
+    [SerializeField] BattleHud hud;
 
-
+    public bool IsPlayerUnit{
+        get{return isPlayerUnit;}
+    }
+    public BattleHud Hud{
+        get{return hud;}
+    }
     public Pokemon Pokemon { get; set; }
 
     Image image;
@@ -30,7 +35,7 @@ public class BattleUnit : MonoBehaviour
             image.sprite = Pokemon.Base.BackSprite;
         else
             image.sprite = Pokemon.Base.FrontSprite;
-
+        hud.SetData(pokemon);
         image.color=originalColor;
         PlayEnterAnimation();
     }
--- Assets/Scripts/Battle/PartyScreen.cs
+++ Assets/Scripts/Battle/PartyScreen.cs
@@ -9,12 +9,14 @@ public class PartyScreen : MonoBehaviour
     [SerializeField] Text messageText;
 
 PartyMemberUI[] memberSlots;
+List<Pokemon> pokemons;
 
 public void Init() {
     memberSlots = GetComponentsInChildren<PartyMemberUI>();
 }
 
 public void SetPartyData(List<Pokemon> pokemons) {
+    this.pokemons=pokemons;
     for (int i = 0; i < memberSlots.Length; i++) {
         if (i < pokemons.Count) 
             memberSlots[i].SetData(pokemons[i]);
@@ -24,4 +26,20 @@ public void SetPartyData(List<Pokemon> pokemons) {
     messageText.text = "Elige un pokemon";
 }
 
+public void UpdateMemberSelection(int selectedMember){
+    for(int i=0;i<pokemons.Count;i++){
+            if(i==selectedMember)
+                memberSlots[i].SetSelected(true);
+                else
+                memberSlots[i].SetSelected(false);
+                
+            
+                
+            }
+    }
+    public void SetMessageText(string message){
+        messageText.text=message;
+    }
 }
+
+
